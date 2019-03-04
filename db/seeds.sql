f71d2f7d0a2e2521693737e87801ff493f26d5b1
lrush85 <lance.rush1@gmail.com>
2019-03-04 16:41:11 -0600

    Initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("bacon cheeseburger", true);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("spicy chicken burger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("double bacon cheeseburger", false);
