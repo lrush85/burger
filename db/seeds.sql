5643f7bf6434c2156ae5ab17a2756885c5b99f11
lrush85 <lance.rush1@gmail.com>
2019-03-09 14:16:23 -0600

    Updated SQL insert data

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,10 +1,20 @@
 USE burgers_db;
 
-INSERT INTO burgers (burger_name, devoured)
-VALUES ("bacon cheeseburger", true);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ("Butterface Burger
+(served
+with butter lettuce)", true);
 
-INSERT INTO burgers (burger_name, devoured)
-VALUES ("spicy chicken burger", false);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ("Summer Thyme Burger", false);
 
-INSERT INTO burgers (burger_name, devoured)
-VALUES ("double bacon cheeseburger", false);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ("Do Brussel Burger
+(comes
+with Brussels sprouts)", false);
